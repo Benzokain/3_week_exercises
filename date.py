55c537f7e991fbb5887edafa3eeed49170c7449b
Yury <night1ps@gmail.com>
2022-09-15 01:11:02 +0300

    date_first

--- date.py
+++ date.py
@@ -2,4 +2,11 @@ from datetime import date, datetime
 
 dt = datetime.now().strftime('%d.%m.%y')
 # dt_str = datetime.
-print(dt)
+asdf = int(date(datetime.now().strftime('%d.%m.%y')))
+print(asdf)
+
+#выводим дату (вчера)
+print()
+
+#выводим текущую дату
+print(datetime.now().strftime('%d.%m'))
